62147996e044bb0ec23e6fa03682994720afe6b8
Zain Ali <bitf18m045@pucit.edu.pk>
2021-11-18 18:38:36 +0500

    Routes Updated

--- src/App.js
+++ src/App.js
@@ -11,9 +11,9 @@ const App = () => {
     <div> 
       <BrowserRouter>
         <Route path="/" component={PrimaryPage} exact/>
-        <Route path="/about#about" component={SecondaryPage} exact/>
-        <Route path="/faq#faq" component={SecondaryPage} exact/>
-        <Route path="/roadmap#roadmap" component={SecondaryPage} exact/>
+        <Route path="/about" component={SecondaryPage} />
+        <Route path="/faq" component={SecondaryPage} />
+        <Route path="/roadmap" component={SecondaryPage} />
       </BrowserRouter>
     </div>
   );
